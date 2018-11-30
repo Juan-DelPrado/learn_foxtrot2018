3bfcb13b935a10dc32ec9b37bbc5c9fd33dec134
traleonester <traleonester@gmail.com>
2018-11-30 11:45:01 -0800

    added JS Intro Challenges questions.

---
+++ JSIntro_challenges.js
@@ -0,0 +1,36 @@
+// If/else challenges 11.29.18
+//
+// Write an if/else statement that considers two variables (numbers) and returns/logs the higher number.  Have “catch all” statement that takes care of numbers that are equal.
+//
+//
+// Arrays
+//
+// Start w/ and empty array
+// Push 4 names into the array
+// Start another empty array
+// Push 4 names into that array
+// Combine the two arrays into a new array without changing the original array
+
+
+
+
+// Loops challenges 11.30.18
+//write 3 loops (1 for loop, 1 while loop and 1 forEach loop) that iterates through an array and returns the same array with every number mult. by 3.
+
+var arr1 = [3, 8, 20, 4, 2]
+
+//expected output [9, 24, 60, 12, 6]
+
+
+// for loop
+
+
+// //while loop
+
+
+// //forEach
+
+
+//stretch 1:  Turn each loop into a function.
+
+//stretch 2:  Refactor your function to return a new array instead of mutating the original arry
